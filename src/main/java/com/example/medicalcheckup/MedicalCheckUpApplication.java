a5b176242691178c1b2cc7ea561f0ea8c86eb4e0
Agatha Evelyn <agathae144@gmail.com>
2024-12-12 11:30:12 +0700

    first commit

---
+++ src/main/java/com/example/medicalcheckup/MedicalCheckUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicalcheckup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalCheckUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalCheckUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medicalcheckup/MedicalCheckUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicalcheckup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalCheckUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
