0845a1a7ebd6a4670206fa22da7b604d0697eaf2
Agatha Evelyn <agathae144@gmail.com>
2024-12-12 11:59:45 +0700

    tambahan html

--- src/main/java/com/example/medicalcheckup/MedicalCheckUpApplication.java
+++ src/main/java/com/example/medicalcheckup/MedicalCheckUpApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MedicalCheckUpApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MedicalCheckUpApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalCheckUpApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/medicalcheckup/MedicalCheckUpApplicationTests.java
+++ src/test/java/com/example/medicalcheckup/MedicalCheckUpApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MedicalCheckUpApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
